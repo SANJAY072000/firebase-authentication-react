8d76960422a44dbecaad2441343d654bd6ad4bdd
sanjay-07_singh <sanjaysinghbisht114@gmail.com>
2019-06-30 21:39:19 +0530

    Done with login button

--- src/Authen.js
+++ src/Authen.js
@@ -13,11 +13,35 @@ var firebaseConfig = {
  firebase.initializeApp(firebaseConfig);
 
 export default class Authen extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      err:''
+    };
+    this.login=this.login.bind(this);
+    // this.signup=this.signup.bind(this);
+    // this.logout=this.logout.bind(this);
+  }
+  login(e){
+    const email=this.refs.email.value,pass=this.refs.pass.value;
+    console.log(email);
+    console.log(pass);
+    const auth=firebase.auth();
+    auth.signInWithEmailAndPassword(email,pass)
+        .catch(err=>{
+          console.log(err.message);
+          this.setState({err:err.message});
+        });
+  }
   render(){
     return(
       <div>
         <input id="email" ref="email" type="email" placeholder="Enter your email"/><br/>
         <input id="pass" ref="pass" type="password" placeholder="Enter your password"/><br/>
+        <p>{this.state.err}</p>
+        <button onClick={this.login}>Log In</button>
+        <button onClick={this.signup}>Sign Up</button>
+        <button onClick={this.logout}>Log Out</button>
 
       </div>
     );
